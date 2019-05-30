02e7bc0cd3696ceb1ef2758ce32cedec76cf0e64
Pau Perez <saulopefa@gmail.com>
2019-05-30 13:52:34 +0200

    Fix EnterpriseMailer specs

--- spec/mailers/enterprise_mailer_spec.rb
+++ spec/mailers/enterprise_mailer_spec.rb
@@ -13,20 +13,24 @@ describe EnterpriseMailer do
 
   describe "#welcome" do
     it "sends a welcome email when given an enterprise" do
-    EnterpriseMailer.welcome(enterprise).deliver
+      performing_deliveries do
+        EnterpriseMailer.welcome(enterprise).deliver
 
-    mail = ActionMailer::Base.deliveries.first
-    expect(mail.subject)
-      .to eq "#{enterprise.name} is now on #{Spree::Config[:site_name]}"
+        mail = ActionMailer::Base.deliveries.first
+        expect(mail.subject)
+          .to eq "#{enterprise.name} is now on #{Spree::Config[:site_name]}"
+      end
     end
   end
 
   describe "#manager_invitation" do
     it "should send a manager invitation email when given an enterprise and user" do
-      EnterpriseMailer.manager_invitation(enterprise, user).deliver
-      expect(ActionMailer::Base.deliveries.count).to eq 1
-      mail = ActionMailer::Base.deliveries.first
-      expect(mail.subject).to eq "#{enterprise.name} has invited you to be a manager"
+      performing_deliveries do
+        EnterpriseMailer.manager_invitation(enterprise, user).deliver
+        expect(ActionMailer::Base.deliveries.count).to eq 1
+        mail = ActionMailer::Base.deliveries.first
+        expect(mail.subject).to eq "#{enterprise.name} has invited you to be a manager"
+      end
     end
   end
 end
